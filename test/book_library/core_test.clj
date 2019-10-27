a2c7a717fe6043f87dddff10d3172cabe3903fe1
Valtteri Pyyhtiä <valtteri@valtteri.io>
2019-10-27 21:06:32 +0200

    Add initial tests

---
+++ test/book_library/core_test.clj
@@ -0,0 +1,15 @@
+(ns book-library.core-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [book-library.core :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "<h1>Hello World!</h1>"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
+
