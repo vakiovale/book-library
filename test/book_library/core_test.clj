c60536b3dc9bdec3f7db248503d0f534e25eac4e
Valtteri Pyyhtiä <valtteri@valtteri.io>
2019-10-27 21:43:17 +0200

    Fix tests

--- test/book_library/core_test.clj
+++ test/book_library/core_test.clj
@@ -7,7 +7,7 @@
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "<h1>Hello World!</h1>"))))
+      (is (= (:body response) "Hello World!"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
