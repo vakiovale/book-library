f11469114c15f49b5b1cad8457c5fa5fc32028cf
Valtteri Pyyhtiä <valtteri@valtteri.io>
2019-11-24 11:05:17 +0200

    Add monger dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject book-library "0.1.0-SNAPSHOT"
   :description "REST API for Book Library application"
   :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+            :url  "http://opensource.org/licenses/MIT"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
@@ -10,10 +10,11 @@
                  [ring/ring-json "0.5.0"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
-                 [ring/ring-devel "1.6.3"]]
+                 [ring/ring-devel "1.6.3"]
+                 [com.novemberain/monger "3.5.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler book-library.core/app}
   :uberjar-name "book-library-standalone.jar"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.2"]]}})
+             :dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                      [ring/ring-mock "0.3.2"]]}})
