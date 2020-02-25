251fadc1a2dba7a7ac5da8b10ae2203f65c36efa
Valtteri Pyyhtiä <valtteri@valtteri.io>
2020-02-25 13:00:04 +0200

    Fix linting problems

--- test/book_library/core_test.clj
+++ test/book_library/core_test.clj
@@ -75,14 +75,14 @@
 (deftest create-book
   (testing "should not create book without authentication"
     (let [response (app (->
-                         (mock/request :post "/books")
-                         (mock/json-body {:name "Bad book!"})))]
+                          (mock/request :post "/books")
+                          (mock/json-body {:name "Bad book!"})))]
       (is (= (:status response) 401))))
   (testing "should create a book"
     (let [response (app (->
-                         (mock/request :post "/books")
-                         (mock/header :authorization test-user-token)
-                         (mock/json-body {:name "My best book"})))]
+                          (mock/request :post "/books")
+                          (mock/header :authorization test-user-token)
+                          (mock/json-body {:name "My best book"})))]
       (is (= (:name (read-book response)) "My best book"))
       (is (= (:user (read-book response)) "user1@example.com"))
       (is (= (:status response) 201)))))
@@ -92,12 +92,12 @@
     (is (= (:status (app (mock/request :get "/books"))) 401)))
   (testing "should not get books with bad token"
     (is (= (:status (app (->
-                          (mock/request :get "/books")
-                          (mock/header :authorization bad-user-token)))) 401)))
+                           (mock/request :get "/books")
+                           (mock/header :authorization bad-user-token)))) 401)))
   (testing "should get list of books"
     (let [response (app (->
-                         (mock/request :get "/books")
-                         (mock/header :authorization test-user-token)))]
+                          (mock/request :get "/books")
+                          (mock/header :authorization test-user-token)))]
       (is (sequential? (read-books response)))))
   (testing "should get only own books"
     (service/create-book {:name "Your book1" :user "user2@example.com"})
@@ -107,8 +107,8 @@
     (service/create-book {:name "No-ones book"})
     (service/create-book {:name "Your book3" :user "user2@example.com"})
     (let [response (app (->
-                         (mock/request :get "/books")
-                         (mock/header :authorization test-user-token)))]
+                          (mock/request :get "/books")
+                          (mock/header :authorization test-user-token)))]
       (is (= (count (map :user (read-books response))) 2))
       (is (every? #(= % "user1@example.com") (map :user (read-books response)))))))
 
@@ -123,41 +123,39 @@
         (is (= (Book/get-id (read-book response)) (Book/get-id book))))))
   (testing "should get status 404 if id does not exist"
     (is (= (:status (app (->
-                          (mock/request :get "/books/notfoundnotfound")
-                          (mock/header :authorization test-user-token)))) 404)))
+                           (mock/request :get "/books/notfoundnotfound")
+                           (mock/header :authorization test-user-token)))) 404)))
   (testing "should get status 404 with somone else's book id"
     (let [book (service/create-book {:name "Book For Dummies" :user "notme@example.com"})]
       (is (= (:status (app (->
-                            (mock/request :get (str "/books/" (Book/get-id book)))
-                            (mock/header :authorization test-user-token)))) 404)))))
+                             (mock/request :get (str "/books/" (Book/get-id book)))
+                             (mock/header :authorization test-user-token)))) 404)))))
 
 (deftest delete-book-by-id
   (testing "should get status 401 without authentication"
     (let [book (service/create-book {:name "Try to delete me!" :user "user1@example.com"})]
-      (let [response (app
-                      (->
-                       (mock/request :delete (str "/books/" (Book/get-id book)))))]
+      (let [response (app (mock/request :delete (str "/books/" (Book/get-id book))))]
         (is (= (:status response) 401)))))
 
   (testing "should get status 200 when deleting a book"
     (let [book (service/create-book {:name "Try to delete me!" :user "user1@example.com"})]
       (let [response (app
-                      (->
-                       (mock/request :delete (str "/books/" (Book/get-id book)))
-                       (mock/header :authorization test-user-token)))]
+                       (->
+                         (mock/request :delete (str "/books/" (Book/get-id book)))
+                         (mock/header :authorization test-user-token)))]
         (is (= (:status response) 200)))))
 
   (testing "should get status 404 when trying to delete someone else's book"
     (let [book (service/create-book {:name "Someone else's book" :user "user2@example.com"})]
       (let [response (app
-                      (->
-                       (mock/request :delete (str "/books/" (Book/get-id book)))
-                       (mock/header :authorization test-user-token)))]
+                       (->
+                         (mock/request :delete (str "/books/" (Book/get-id book)))
+                         (mock/header :authorization test-user-token)))]
         (is (= (:status response) 404)))))
 
   (testing "should get status 404 when trying to delete book that does not exist"
     (let [response (app
-                    (->
-                     (mock/request :delete (str "/books/" (java.util.UUID/randomUUID)))
-                     (mock/header :authorization test-user-token)))]
+                     (->
+                       (mock/request :delete (str "/books/" (java.util.UUID/randomUUID)))
+                       (mock/header :authorization test-user-token)))]
       (is (= (:status response) 404)))))
